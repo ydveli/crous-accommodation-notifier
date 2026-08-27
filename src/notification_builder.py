1f06027e1dbecd28b99970a66de0dd109d287aba
DIA Saidou <dia56g@gmail.com>
2026-08-27 09:52:48 +0200

    Fix notification message formatting and content

--- src/notification_builder.py
+++ src/notification_builder.py
@@ -15,25 +15,22 @@ class NotificationBuilder:
             return None
 
         if not accommodations:
-            message = "Aucun logement trouvé. Voici une liste des ponts de France où vous pourriez dormir : https://fr.wikipedia.org/wiki/Liste_de_ponts_de_France"
+            message = "Aucun logement trouve."
         else:
             s = "s" if len(accommodations) > 1 else ""
             verb = "sont" if len(accommodations) > 1 else "est"
-            message = f"Bonne nouvelle 😯, {len(accommodations)} logement{s} {verb} disponible{s} : \n "
+            message = f"Bonne nouvelle, {len(accommodations)} logement{s} {verb} disponible{s} :\n"
 
         def format_one_accommodation(accommodation: Accommodation):
             price = (
-                f"{accommodation.price}€"
+                f"{accommodation.price}EUR"
                 if isinstance(accommodation.price, float)
                 else accommodation.price
             )
-
             link = f"https://trouverunlogement.lescrous.fr/tools/36/accommodations/{accommodation.id}"
-
-            return f"[*{accommodation.title}*]({link}) ({price})"
+            return f"{accommodation.title} ({price})\n{link}"
 
         message += "\n\n".join(map(format_one_accommodation, accommodations))
-
         message += f"\n\n{search_results.search_url}"
 
         return Notification(message=message)
